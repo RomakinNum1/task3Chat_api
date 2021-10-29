a11cf4bc5c542aa7992a7683203fb497aa1ab18a
RomakinNum1 <stud115405@vyatsu.ru>
2021-10-29 15:50:28 +0300

    Исправление ошибок сервера и вынос переменных в .env

--- server.php
+++ server.php
@@ -4,10 +4,11 @@ use Workerman\Worker;
 
 require_once __DIR__ . '/composer/vendor/autoload.php';
 
-$wsWorker = new Worker('websocket://0.0.0.0:2346');
+$wsWorker = new Worker('websocket://0.0.0.0:2346');//websocket://0.0.0.0:2346 $_ENV['SERVER_CONNECTION_URL']
 $wsWorker->count = 4;
 
 $Connections = [];
+$CountOfUserConnection = [];
 
 $wsWorker->onConnect = function ($connection) {
     echo 'New connection
@@ -16,7 +17,7 @@ $wsWorker->onConnect = function ($connection) {
 
 $wsWorker->onMessage = function ($connection, $data) use ($wsWorker) {
     $arrayData = json_decode($data);
-    global $Connections;
+    global $Connections, $CountOfUserConnection;
 
     if ($arrayData->type === 'sendMessage') {
         foreach ($wsWorker->connections as $clientConn) {
@@ -30,9 +31,11 @@ $wsWorker->onMessage = function ($connection, $data) use ($wsWorker) {
         if (array_search($arrayData->name, $Connections)!==false) {
             $arrayData->usersArr = $Connections;
             $connection->send(json_encode($arrayData));
+            $CountOfUserConnection[$arrayData->name]++;
             return;
         }
         $Connections[] = $arrayData->name;
+        $CountOfUserConnection[$arrayData->name] = 1;
         $arrayData->usersArr = $Connections;
 
         foreach ($wsWorker->connections as $clientConn) {
@@ -43,6 +46,9 @@ $wsWorker->onMessage = function ($connection, $data) use ($wsWorker) {
 
     if ($arrayData->type === 'removeUser') {
         unset($Connections[array_search($arrayData->name, $Connections)]);
+
+        $CountOfUserConnection[$arrayData->name]--;
+        if($CountOfUserConnection[$arrayData->name] !== 0) return;
 
         $arrayData->usersArr = $Connections;
 
